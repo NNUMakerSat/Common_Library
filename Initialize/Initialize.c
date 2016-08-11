3dcdaec0f27e02ed1cbfe1c8290bc0780871d379
Aaron Ewing <aaronewing@nnu.edu>
2016-08-11 13:27:26 -0600

    creating inialize

---
+++ Initialize/Initialize.c
@@ -0,0 +1,10 @@
+/*
+ * Initialize.c
+ *
+ *  Created on: Aug 11, 2016
+ *      Author: aaronewing
+ */
+
+
+
+
---
+++ Initialize/Initialize.h
@@ -0,0 +1,15 @@
+/*
+ * Initialize.h
+ *
+ *  Created on: Aug 11, 2016
+ *      Author: aaronewing
+ */
+
+#ifndef INITIALIZE_H_
+#define INITIALIZE_H_
+
+
+
+
+
+#endif /* INITIALIZE_H_ */
