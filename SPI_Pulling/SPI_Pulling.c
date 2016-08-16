6a285d3833773ad168ce45658706d633c04cd21d
Aaron Ewing <aaronewing@nnu.edu>
2016-08-16 12:54:12 -0600

    added header and source file

--- SPI_Pulling/SPI_Pulling.c
+++ SPI_Pulling/SPI_Pulling.c
@@ -4,7 +4,24 @@
  *  Created on: Aug 16, 2016
  *      Author: aaronewing
  */
+#include <msp430.h>
+#include <stdint.h>
 
+/* P1.5 - UCA0CLK
+*  P1.6 - SYNC
+*  P2.0 - SIMO
+*  P2.1 - SOMI
+*/
 
+void init_SPI (void) {
+	// Configure Primary Function Pins
+	P1SEL1 |= BIT5;                           // USCI_A0 operation (P1.5)
+	P2SEL0 |= BIT0 | BIT1;                    // USCI_A0 operation (P2.0 & P2.1)
+	PJSEL0 |= BIT4 | BIT5;                    // For XT1
 
+	// configure P1.6 (SYNC) as GPIO used to enable SPI write to DAC
+	P1SEL0 &= ~BIT6;
+	P1SEL1 &= ~BIT6;
+	P1DIR |= BIT6;
+}
 
--- SPI_Pulling/SPI_Pulling.h
+++ SPI_Pulling/SPI_Pulling.h
@@ -4,12 +4,14 @@
  *  Created on: Aug 16, 2016
  *      Author: aaronewing
  */
+#include <msp430.h>
+#include <stdint.h>
 
 #ifndef SPI_PULLING_H_
 #define SPI_PULLING_H_
 
-
-
-
+void init_SPI (void);		// pin select?
+void write_SPI (uint8_t);
+uint8_t read_SPI (void);
 
 #endif /* SPI_PULLING_H_ */
