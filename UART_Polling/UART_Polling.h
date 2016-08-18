221eb2df6c743cbee7d0caf7c0c6355d62cb785f
Aaron Ewing <aaronewing@nnu.edu>
2016-08-18 14:48:50 -0600

    update

--- UART_Polling/UART_Polling.h
+++ UART_Polling/UART_Polling.h
@@ -7,6 +7,7 @@
 #ifndef UART_POLLING_H_
 #define UART_POLLING_H_
 
+void init_UART (bool baud_Rate, bool pin_Setting);	// initalizes UART clk rate and which pins are being used
 void write_UART (uint8_t TX_Data);				// writes 8 bits with SPI
 uint8_t read_UART (void);
 
